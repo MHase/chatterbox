95e33ee42eabf71c5df3fa8491487742426da8be
Matt <matthewprimpas@gmail.com>
2018-08-12 14:23:41 -0400

    Make titles relevant to their pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,14 @@ module ApplicationHelper
     image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}?s=#{opts.delete(:size) { 40 }}",
               opts
   end
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "ChatterBox"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
