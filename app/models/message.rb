bf95f35327b8bc4a21a27ad3f9117973d1641355
Matt <matthewprimpas@gmail.com>
2018-08-21 21:07:39 -0400

    Link User location to Google Maps

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,4 +6,15 @@ class Message < ApplicationRecord
 
   after_create_commit { MessageBroadcastJob.perform_later(self) }
 
+  def location
+    if @user.city && @user.state != nil
+      "#{@user.city}" + "," + "#{@user.state}"
+    else
+      @user.city || @user.state
+    end
+  end
+
+  # TODO make it so messages are deleted after 24 hours of posting.
+  # TODO make messages specific to user's geographical location i.e. only show messages from NYC users when the user is in NYC.
+
 end
